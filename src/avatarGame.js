9dd099332bee3425f93d5d75cd46f80463123662
Faith <seventanyb@gmail.com>
2018-06-22 16:08:37 +0800

    avatar bg

--- src/avatarGame.js
+++ src/avatarGame.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import _ from 'lodash';
+import GameItem from './gameItem.js';
 
+const baseImgPath = '/static/img';
+const picBgColumns = [
+  { title: 'Front-End'},
+  { title: 'HTML' },
+  { title: 'CSS' },
+  { title: 'Javascript' },
+  { title: 'React' },
+  { title: 'Redux/Mobx' },
+  { title: 'Antd' },
+  { title: 'Node.js/Meteor' },
+  { title: 'Tools' },
+]
 class AvatarGame extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blankPic: 9,
+    }
+  }
+  checkShowImg = (index) => {
+    let { blankPic } = this.state;
+    if(index === blankPic) {
+      return null
+    }
+    return `${baseImgPath}/${index}.gif`;
+  }
   render() {
     return (
       <div className="avatar-wrap">
-        <div className="pic-game">
+        <div className="game-box center">
+          { _.map(picBgColumns, (item, i) => (
+            <GameItem key={i} imgSrc={this.checkShowImg(i+1)} title={item.title} />
+          ))
+          }
         </div>
       </div>
     );
---
+++ src/gameItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class GameItem extends React.Component {
+  render() {
+    let { title, imgSrc } = this.props;
+    return (
+      <div className={`game-box-item ${!imgSrc ? 'game-box-item-text' : ''}`}>
+        { imgSrc ?  (
+          <img src={imgSrc} alt="worl" />
+          ) : title
+        }
+      </div>
+    );
+  }
+}
+
+export default GameItem;
